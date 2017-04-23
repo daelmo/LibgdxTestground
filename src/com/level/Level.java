1ac183f6a03bc3527c8ca31a80ab13fe396610f4
Josephine Rehak <justallphine@googlemail.com>
2017-04-23 21:34:10 +0200

    introduced properties file for constants

--- src/com/level/Level.java
+++ src/com/level/Level.java
@@ -4,61 +4,58 @@ import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 
 /**
- *
  * @author fine
  */
 public class Level {
 
-    private static Level instance = null;
-    public Tile[][] GameMap;
-    public Group MapGroup = new Group();
+	private static Level instance = null;
+	public Tile[][] GameMap;
+	public Group MapGroup = new Group();
 
-    public static final int LEVEL_HEIGHT = 10;
-    public static final int LEVEL_WIDTH = 10;
-    public static final int FLOOR_ZINDEX = 0;
-    public static final int FURNITURE_ZINDEX = 1;
-    public static final int OBJECT_ZINDEX = 2;
-    public Group PersonGroup = new Group();
+	public static final int FLOOR_ZINDEX = 0;
+	public static final int FURNITURE_ZINDEX = 1;
+	public static final int OBJECT_ZINDEX = 2;
+	public Group PersonGroup = new Group();
 
-    protected Level() {
-	GameMap = new Tile[LEVEL_WIDTH][LEVEL_HEIGHT];
-	for (int x = 0; x < LEVEL_WIDTH; x++) {
-	    for (int y = 0; y < LEVEL_HEIGHT; y++) {
-		Tile t = new Tile(x, y, 3);
-		GameMap[x][y] = t;
-		MapGroup.addActor(t);
-	    }
+	protected Level(int size) {
+		GameMap = new Tile[size][size];
+		for (int x = 0; x < size; x++) {
+			for (int y = 0; y < size; y++) {
+				Tile t = new Tile(x, y, 3);
+				GameMap[x][y] = t;
+				MapGroup.addActor(t);
+			}
+		}
 	}
-    }
 
-    public static Level getInstance() {
-	if (instance == null) {
-	    instance = new Level();
+	public static Level getInstance(int size) {
+		if (instance == null) {
+			instance = new Level(size);
+		}
+		return instance;
 	}
-	return instance;
-    }
 
-    public void addToStage(Stage stage) {
-	stage.addActor(MapGroup);
-	stage.addActor(PersonGroup);
-    }
-
-    public void removeFromStage() {
-	MapGroup.remove();
-	PersonGroup.remove();
-    }
-
-    public boolean isTraversable(int x, int y) {
-	Tile t = GameMap[x][y];
-	for (boolean traversable : t.isTraversable) {
-	    if (traversable == false) {
-		return false;
-	    }
+	public void addToStage(Stage stage) {
+		stage.addActor(MapGroup);
+		stage.addActor(PersonGroup);
 	}
-	return true;
-    }
 
-    public void setTraversable(int x, int y, int z, boolean bool) {
-	GameMap[x][y].setTraversable(z, bool);
-    }
+	public void removeFromStage() {
+		MapGroup.remove();
+		PersonGroup.remove();
+	}
+
+	public boolean isTraversable(int x, int y) {
+		Tile t = GameMap[x][y];
+		for (boolean traversable : t.isTraversable) {
+			if (traversable == false) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public void setTraversable(int x, int y, int z, boolean bool) {
+		GameMap[x][y].setTraversable(z, bool);
+	}
 }
