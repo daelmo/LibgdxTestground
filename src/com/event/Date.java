5c76fb9428f94ef43936bebe0c81fb63731d0f7a
Josephine Rehak <justallphine@googlemail.com>
2017-04-25 14:03:38 +0200

    Date directory

---
+++ src/com/event/Date.java
@@ -0,0 +1,32 @@
+package com.event;
+
+public class Date {
+	public TimeController.Season season;
+	public int day;
+	public int year;
+	public static final int DAYS_PER_SEASON = 30;
+	public static final int SECONDS_PER_DAY = 10;
+
+	public Date(int year, TimeController.Season season, int day){
+		this.year = year;
+		this.season = season;
+		this.day = day;
+	}
+
+	public Date getNextDate(){
+		if(day++ <= DAYS_PER_SEASON){
+			return new Date(year, season, day++);
+		}
+
+		if(season.ordinal() + 1 <= TimeController.Season.values().length){
+			return new Date(year, TimeController.Season.values()[season.ordinal() + 1], 1);
+		}
+
+		return new Date(year++, TimeController.Season.spring, 1);
+	}
+
+	@Override
+	public String toString(){
+		return day + " " + season + " " + year;
+	}
+}
---
+++ src/com/event/TimeController.java
@@ -0,0 +1,33 @@
+package com.event;
+
+import static com.event.Date.SECONDS_PER_DAY;
+
+public class TimeController {
+
+	public enum Season {
+		spring, summer, autumn, winter
+	}
+
+	private double absoluteTime =0; //passed game absoluteTime in seconds
+	public Date currentDate;
+	public float currentSeconds;
+
+	public TimeController(Season season){
+		this.currentDate = new Date(0, season, 1);
+		this.currentSeconds = 0;
+	}
+
+	public void addTime(float delta){
+		absoluteTime += delta;
+		currentSeconds += delta;
+		if(currentSeconds >= SECONDS_PER_DAY){
+			currentDate = currentDate.getNextDate();
+			currentSeconds = currentSeconds % SECONDS_PER_DAY;
+		}
+	}
+
+	public double getAbsoluteTime(){
+		return absoluteTime;
+	}
+
+}
