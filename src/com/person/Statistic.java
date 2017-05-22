2c04f9718a922119d95e9274639dd8817b0696ae
Josephine Rehak <justallphine@googlemail.com>
2017-05-22 12:33:04 +0200

    implemented hunger

--- src/com/person/Statistic.java
+++ src/com/person/Statistic.java
@@ -3,19 +3,28 @@ package com.person;
 public class Statistic {
 	private Person person;
 	public float walkingSpeed = 60f;
+	public float hungerRate = 3;
 	public float health = FULLHEALTH;
+	public float satiety = FULLSATIETY;
 
 
 	public static final float FULLHEALTH = 100;
 	public static final float UNCONSCIOUS = 35;
 	public static final float DEAD = 0;
 
+	public static final float FULLSATIETY = 100;
+	public static final float LOWSATIETY = 35;
+	public static final float NOSATIETY = 0;
+
 	public Statistic(Person person){
 		this.person = person;
 	}
 
 	public void update( float delta){
-		takeDamage( delta * 1);
+		if(satiety <= 0){
+			takeDamage( delta * 1);
+		}
+		beHungry();
 	}
 
 	public void takeDamage(float damage){
@@ -26,6 +35,10 @@ public class Statistic {
 		if(health < DEAD && person.getState() != State.dead){
 			person.setState(State.dead);
 		}
+	}
+
+	private void beHungry(){
+		satiety = Math.max(satiety - hungerRate, 0);
 	}
 
 }
