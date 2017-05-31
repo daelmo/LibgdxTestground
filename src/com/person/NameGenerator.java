bca66fb67e494c1689bb11d22f1247ac72bf517d
daelmo <justallphine@googlemail.com>
2017-05-31 19:01:12 +0200

    names got random and less likely doubled

--- src/com/person/NameGenerator.java
+++ src/com/person/NameGenerator.java
@@ -7,28 +7,34 @@ import com.badlogic.gdx.utils.JsonValue;
 import com.event.TimeController;
 import javafx.stage.Stage;
 
-import java.util.Random;
+import java.util.*;
 
 public class NameGenerator {
-	private String[] femalePersonNames;
-	private String[] malePersonNames;
+	private ArrayList<String> femalePersonNames;
+	private ArrayList<String> malePersonNames;
+	private ListIterator<String> femalePersonNameIterator;
+	private ListIterator<String> malePersonNameIterator;
 
 	public NameGenerator() {
 		JsonValue json = new JsonReader().parse(Gdx.files.internal("data/names.json"));
 		JsonValue.JsonIterator it = json.iterator();
-		femalePersonNames = it.next().asStringArray();
-		malePersonNames = it.next().asStringArray();
+		femalePersonNames = new ArrayList<>(Arrays.asList(it.next().asStringArray()));
+		malePersonNames = new ArrayList<>(Arrays.asList(it.next().asStringArray()));
+		Collections.shuffle(femalePersonNames);
+		Collections.shuffle(malePersonNames);
+		femalePersonNameIterator = femalePersonNames.listIterator();
+		malePersonNameIterator = malePersonNames.listIterator();
 	}
 
 
 	public String generatePersonName(Gender gender) {
 		String name;
-		Random ran = new Random();
-		int ranInt = ran.nextInt(femalePersonNames.length);
 		if(gender == Gender.FEMALE){
-			name = femalePersonNames[ranInt];
-		} else {
-			name = malePersonNames[ranInt];
+			if(!femalePersonNameIterator.hasNext()){femalePersonNameIterator = femalePersonNames.listIterator();}
+			name = femalePersonNameIterator.next();
+		}else {
+			if(!malePersonNameIterator.hasNext()){malePersonNameIterator = malePersonNames.listIterator();}
+			name = malePersonNameIterator.next();
 		}
 		return name;
 	}
