435fe3061af62babd45baf88de950d0cc02d422a
daelmo <justallphine@googlemail.com>
2017-07-02 00:33:36 +0200

    random position now on 10x10 grit

--- src/com/level/Position.java
+++ src/com/level/Position.java
@@ -14,11 +14,11 @@ public class Position implements Comparable<Position>{
 	}
 
 	public int getIntX() {
-		return (int) x;
+		return Math.round(x);
 	}
 
 	public int getIntY() {
-		return (int) y;
+		return Math.round(y);
 	}
 
 	public float getFloatX() {
@@ -65,8 +65,8 @@ public class Position implements Comparable<Position>{
 
 	public static Position getRandomPosition(){
 		Random random = new Random();
-		int x = random.nextInt( Constants.TILE_WIDTH *10) +1;
-		int y = random.nextInt(Constants.TILE_HEIGHT *10) +1;
+		int x = random.nextInt( 10) * Constants.TILE_WIDTH +1;
+		int y = random.nextInt(10) * Constants.TILE_HEIGHT +1;
 		return new Position(x,y);
 	}
 
