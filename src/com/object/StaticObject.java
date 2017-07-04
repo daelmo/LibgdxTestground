eddc78a7cbd37add029345e0b0047396a83ed4d0
daelmo <justallphine@googlemail.com>
2017-07-04 14:24:19 +0200

    implemented offset

--- src/com/object/StaticObject.java
+++ src/com/object/StaticObject.java
@@ -3,26 +3,28 @@ package com.object;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.game.Constants;
-import com.game.Selectable;
 import com.level.MapSprite;
 
-public class StaticObject extends Actor implements Selectable{
+public class StaticObject extends Actor{
 
 	private int imageWidth, imageHeight;
 	private int originX, originY;
+	private int offsetX, offsetY;
 	private boolean isTraversable;
 	private String spritePath;
 	private MapSprite sprite;
 	private int posX, posY;
 	private int[][] blockedArea;
 
-	public StaticObject createCopyAt(int X, int Y){
+	public StaticObject createDeepCopyAt(int X, int Y){
 		StaticObject object = new StaticObject();
 		object.spritePath = this.spritePath;
 		object.imageWidth = this.imageWidth;
 		object.imageHeight = this.imageHeight;
 		object.originX = this.originX;
 		object.originY = this.originY;
+		object.offsetX = this.offsetX;
+		object.offsetY = this.offsetY;
 		object.isTraversable = this.isTraversable;
 		object.blockedArea = this.blockedArea;
 		object.posX = X;
@@ -32,7 +34,7 @@ public class StaticObject extends Actor implements Selectable{
 
 	public void draw(Batch batch, float alpha) {
 		sprite = new MapSprite(spritePath, imageWidth, imageHeight);
-		sprite.setPosition(posX * Constants.TILE_WIDTH, posY * Constants.TILE_HEIGHT);
+		sprite.setPosition(posX * Constants.TILE_WIDTH + offsetX, posY * Constants.TILE_HEIGHT + offsetY);
 		sprite.setOrigin(originX,originY);
 		sprite.draw(batch);
 	}
